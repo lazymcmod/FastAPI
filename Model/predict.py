25af62225e301ebdd4734806051297cf94153c74
Ayush Singh <lazymcmod1234@gmail.com>
2026-08-24 19:26:30 +0530

    convert user input to dict

--- Model/predict.py
+++ Model/predict.py
@@ -10,6 +10,6 @@ MODEL_VERSION = '1.0.0'
 
 def predict_output(user_input: dict):
 
-    input_df = pd.DataFrame(user_input)
+    input_df = pd.DataFrame([user_input])
     output = model.predict(input_df)[0]
     return output
