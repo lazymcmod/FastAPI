f94d4130ec76b717438e086f4a3095eb8ad78f44
Ayush Singh <lazymcmod1234@gmail.com>
2026-08-24 19:57:24 +0530

    fixing some error in app.py

--- Model/app.py
+++ Model/app.py
@@ -24,17 +24,19 @@ def health():
 @app.post('/predict')
 def predict_premium(data: UserInput):
 
-    user_input = ([{
+    user_input = {
         'bmi': data.bmi,
         'age_group': data.age_group,
         'lifestyle_risk': data.lifestyle_risk,
         'city_tier': data.city_tier,
         'income_lpa': data.income_lpa,
         'occupation': data.occupation
-
-    }])
-
-    prediction = predict_output(user_input)
-    return {
-        "predicted_category": str(prediction[0])
     }
+    try:
+        prediction = predict_output(user_input)
+        return {
+        JSONResponse(status_code=200,content={'response': prediction})
+        }
+    except Exception as e:
+
+        return JSONResponse(status_code=500,content=str(e))
