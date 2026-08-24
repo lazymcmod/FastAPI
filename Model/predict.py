3931ec78fdd36db8b450ed440ad855796f1a44ae
Ayush Singh <lazymcmod1234@gmail.com>
2026-08-24 19:56:52 +0530

    adding some important code in predict.py

--- Model/predict.py
+++ Model/predict.py
@@ -1,6 +1,7 @@
 import pickle
 import pandas as pd
 
+
 # import the ml model
 with open('Model/model.pkl','rb') as f:
     model = pickle.load(f)
@@ -8,8 +9,22 @@ with open('Model/model.pkl','rb') as f:
 # ML flow
 MODEL_VERSION = '1.0.0'
 
+# get class labels from model 
+class_labels = model.classes_.tolist()
+
+
 def predict_output(user_input: dict):
 
-    input_df = pd.DataFrame([user_input])
-    output = model.predict(input_df)[0]
-    return output
+    df = pd.DataFrame([user_input])
+    predicted_class = model.predict(df)[0]
+
+    probablities = model.predict_proba(df)[0]
+    confidence = max(probablities)
+
+    class_probs = dict(zip(class_labels,map(lambda p: round(p, 4),probablities)))
+
+    return {
+        "predicted_cateory": predicted_class,
+        "confidence": round(confidence, 4),
+        "class_probablities": class_probs
+    }
