900d0ca3d8bdee6ddcb63016054a844c4d05be2e
Ayush Singh <lazymcmod1234@gmail.com>
2026-08-24 19:23:54 +0530

    remove and make much better app.py

--- Model/app.py
+++ Model/app.py
@@ -2,140 +2,10 @@ from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel,Field,computed_field
 from typing import Literal,Annotated
-import pandas as pd
-import pickle
-
-# import the ml model
-with open('Model/model.pkl','rb') as f:
-    model = pickle.load(f)
-
-# ML flow
-MODEL_VERSION = '1.0.0'
+from Schema.user_input import UserInput
+from Model.predict import predict_output,model,MODEL_VERSION
 
 app = FastAPI()
-
-# Tier 1 Cities
-tier_1_cities = [
-    "Mumbai",
-    "Delhi",
-    "Bangalore",
-    "Chennai",
-    "Kolkata",
-    "Hyderabad",
-    "Pune"
-]
-
-
-# Tier 2 Cities
-tier_2_cities = [
-    "Jaipur",
-    "Chandigarh",
-    "Indore",
-    "Lucknow",
-    "Patna",
-    "Ranchi",
-    "Visakhapatnam",
-    "Coimbatore",
-    "Bhopal",
-    "Nagpur",
-    "Vadodara",
-    "Surat",
-    "Rajkot",
-    "Jodhpur",
-    "Raipur",
-    "Amritsar",
-    "Varanasi",
-    "Agra",
-    "Dehradun",
-    "Mysore",
-    "Jabalpur",
-    "Guwahati",
-    "Thiruvananthapuram",
-    "Ludhiana",
-    "Nashik",
-    "Allahabad",
-    "Udaipur",
-    "Aurangabad",
-    "Hubli",
-    "Belgaum",
-    "Salem",
-    "Vijayawada",
-    "Tiruchirappalli",
-    "Bhavnagar",
-    "Gwalior",
-    "Dhanbad",
-    "Bareilly",
-    "Aligarh",
-    "Gaya",
-    "Kozhikode",
-    "Warangal",
-    "Kolhapur",
-    "Bilaspur",
-    "Jalandhar",
-    "Noida",
-    "Guntur",
-    "Asansol",
-    "Siliguri"
-]
-
-
-# pydantic models to validate incomig data 
-class UserInput(BaseModel):
-
-    age: Annotated[int, Field(..., gt=0,lt=120,description='Age of the user')]
-    weight: Annotated[float, Field(..., gt=0,description='Weight of the user')]
-    height: Annotated[float, Field(..., gt=0,lt=2.5,description='height of the user')]
-    income_lpa: Annotated[float, Field(..., gt=0,description='Annual salary of the user')]
-    smoker: Annotated[bool, Field(...,description='Is user a smoker ')]
-    city: Annotated[str, Field(...,description='The city of the user')] 
-    occupation: Annotated[Literal['retired', 'freelancer', 'student', 'government_job',
-       'business_owner', 'unemployed', 'private_job'], Field(...,description='Occupation of the user')]
-
-
-    @computed_field
-    @property
-    def normalize_city(cls,v: str) -> str:
-        v = v.strip().title()
-        return v
-
-    @computed_field
-    @property
-    def bmi(self) -> float:
-        return self.weight/(self.height**2)
-
-    @computed_field
-    @property
-    def lifestyle_risk(self) -> str:
-        if  self.smoker and self.bmi > 30:
-            return "high"
-        elif  self.smoker and self.bmi > 27:
-            return "medium"
-        else:
-            return "low"
-
-    @computed_field
-    @property
-    def age_group(self) -> str:
-
-        if self.age < 25:
-                return "young"
-        elif self.age < 45:
-                return"adult"
-        elif self.age < 60:
-                return "middle-aged"
-        return "Senior"
-
-    @computed_field
-    @property
-    def city_tier(self) -> str:
-
-        if self.city in tier_1_cities:
-            return 1
-        elif self.city in tier_2_cities:
-            return 2
-        else:
-            return 3 
-
 
 # human readable
 @app.get('/')
@@ -154,7 +24,7 @@ def health():
 @app.post('/predict')
 def predict_premium(data: UserInput):
 
-    input_df = pd.DataFrame([{
+    user_input = ([{
         'bmi': data.bmi,
         'age_group': data.age_group,
         'lifestyle_risk': data.lifestyle_risk,
@@ -164,8 +34,7 @@ def predict_premium(data: UserInput):
 
     }])
 
-    prediction = model.predict(input_df)
-
+    prediction = predict_output(user_input)
     return {
         "predicted_category": str(prediction[0])
     }
