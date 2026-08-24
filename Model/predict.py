ce36668b3b3bed1b2939b5279b5ac80c7dc8a4f0
Ayush Singh <lazymcmod1234@gmail.com>
2026-08-24 19:24:24 +0530

    adding model to predict.py

---
+++ Model/predict.py
@@ -0,0 +1,15 @@
+import pickle
+import pandas as pd
+
+# import the ml model
+with open('Model/model.pkl','rb') as f:
+    model = pickle.load(f)
+
+# ML flow
+MODEL_VERSION = '1.0.0'
+
+def predict_output(user_input: dict):
+
+    input_df = pd.DataFrame(user_input)
+    output = model.predict(input_df)[0]
+    return output
